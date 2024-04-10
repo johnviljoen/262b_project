01fcd95d0c54103477794a68c34d88118bf2ae09
johnviljoen <johnviljoen2@gmail.com>
2024-04-10 15:18:23 -0700

    progress

--- imitate.py
+++ imitate.py
@@ -1,8 +1,5 @@
 """
-This script demonstrates DPCs failure on a system, which IPOPT succeeds on in ipopt.py
-
-whats more is that without the horizon being tiny at 2 the loss will become infinite and 
-training will stall.
+This script will purely imitate the MPC solution at every timestep
 """
 
 import jax
@@ -21,6 +18,7 @@ import jax.numpy as jnp
 from utils.mlp import init_pol, pol_inf
 from utils.opt import adagrad, clip_grad_norm
 import dynamics
+from mpc import MPC
 
 # f = dynamics.get("L_SIMO_RD1") # 24 loss
 # f = dynamics.get("L_SIMO_RD2") # 124 loss
@@ -72,7 +70,7 @@ if __name__ == "__main__":
 
     train_data = 3.0 * np.random.randn(1, 3333, nx)
     num_epochs = 400 # 400
-    hzn = 3
+    hzn = 4
 
     for epoch in range(num_epochs):
         for b_s in train_data:  # shape = (1, 3333, 2)
@@ -85,7 +83,8 @@ if __name__ == "__main__":
     #         loss, opt_s = step(epoch, opt_s, b_s, hzn=hzn)
     #     print(f"epoch: {epoch}, loss: {loss}")
 
-    eval_data = 3.0 * np.random.randn(1, nx)
+    # eval_data = 3.0 * np.random.randn(1, nx) generated the below:
+    eval_data = jnp.array([1.59609801, 1.51405802, 4.63639117])
     eval_hzn = 10
     eval_loss = b_cost(pol_s, eval_data, eval_hzn)
 
@@ -102,7 +101,47 @@ if __name__ == "__main__":
     
     b_a_N, b_s_N = jnp.stack(b_a_N), jnp.stack(b_s_N)
     (R * jnp.sum(b_a_N**2) + Q * jnp.sum(b_s_N**2)) / b
+    print(f'dpc loss: {loss}')
+
+    ### MPC
+    mpc = MPC(N=hzn, nx=nx, nu=nu, ny=3, f=f)
+    # Q = 10.0                # state loss
+    # R = 0.0001              # action loss
+    u_N, x_N = [], []
+    x = eval_data
+    loss = 0.0
+    for _ in range(10):
+        u = mpc(x)
+        x_kp1 = f(x, u)
+        loss += (R * jnp.sum(u**2) + Q * jnp.sum(x_kp1**2)) / hzn
+        x = x_kp1
+        u_N.append(u); x_N.append(x)
+    
+    u_N, x_N = jnp.stack(u_N), jnp.stack(x_N)
+    (R * jnp.sum(u_N**2) + Q * jnp.sum(x_N**2)) / hzn
+    print(f'mpc loss: {loss}')
+
     print('fin')
 
+    ### interesting!! we have seen that even the MPC needs a horizon of longer 
+    # than 3 to achieve stability in its third state. This makes perfect sense!!
+    # as the system is relative degree 3, the predictive step needs to be at least
+    # as many steps as relative degrees to control all states!!!. Therefore 4 is
+    # required!!
+
+    # The problem is that we only can have a finite cost DPC with a small number of steps
+    # at least initially. Also the DPC fails more and more the further along the 
+    # chain of integrators we get:
+    # b_s_N[-1, 1, :]
+    # Array([225.7935   ,  44.549297 ,   3.4590864], dtype=float32)
+    # b_s_N[-1, 10, :]
+    # Array([67.69244   ,  9.557059  , -0.13979149], dtype=float32)
+    # b_s_N[-1, 11, :]
+    # Array([-71.5085   , -19.557402 ,  -2.7800481], dtype=float32)
+
+    # MPC doesnt have this problem, so lets see if we can imitate to get some of its
+    # performance benefits!
+
     
+
 
--- mpc.py
+++ mpc.py
@@ -2,13 +2,25 @@ from jax import jit, grad, jacrev, jacfwd
 import jax.numpy as jnp
 import dynamics
 from cyipopt import minimize_ipopt
+import numpy as np
 
 class MPC:
+
+    """
+    Forms the MPC problem and solves it of form:
+
+        min sum { x.T Q x + u.T @ R u }
+        x,u
+        s.t. h(x) = 0
+             g(x) < 0
+    
+    """
+
     def __init__(
-        self,
-        N, nx, nu, ny,
-        f
-        ) -> None:
+            self,
+            N, nx, nu, ny,
+            f
+        ) -> None:  
 
         self.N, self.nx, self.nu, self.ny = N, nx, nu, ny
         self.f = f
@@ -52,7 +64,7 @@ class MPC:
             hess=self.l_hess,
             constraints=constraints,
             tol=1e-6,
-            options={'disp': 5, 'maxiter': 100}
+            options={'disp': 0, 'maxiter': 100}
         )
 
         print('fin')
@@ -60,6 +72,12 @@ class MPC:
     def __call__(self, x):
 
         # equality constraint definitions need to be updated with changed values for current x
+        # this implicit x application is disgusting but necessary to work with the jax jitted
+        # functions and cyipopt which cannot pass arguments to the constraints beyond  the 
+        # optimization variables, there is a clean way to do this by creating another function
+        # which passes *args and returns a new constraints list, but I think overcomplicated 
+        # for just an MPC like this.
+
         h_jit = lambda z: self.h_jit(z, x) 
         h_jac = lambda z: self.h_jac(z, x)
         h_hessvp = lambda z, v: jnp.sum(self.h_hess(z, x) * v[:, None, None], axis=0)
@@ -71,21 +89,24 @@ class MPC:
         
         # call minimize once for cython compilation
         result = minimize_ipopt(
-            fun=self.l_jit, 
-            x0=self.z_init, 
-            jac=self.l_grad, 
-            hess=self.l_hess, 
+            fun=self.l_jit,
+            x0=self.z_init,
+            jac=self.l_grad,
+            hess=self.l_hess,
             constraints=constraints, 
-            tol=1e-6, options={'disp': 5, 'maxiter': 100}
+            tol=1e-6, options={'disp': 0, 'maxiter': 100}
         )
 
-        # Extract solution
+        # Extract solution and assign warm start z_init
+        self.z_init = result.x
+        # warm = result.x[self.nx:self.N*self.nx]
+
         sol_x = result.x[:self.N*self.nx].reshape(self.N, self.nx)
         sol_u = result.x[self.N*self.nx:].reshape(self.N-1, self.nu)
+        # print('Optimal state trajectory:', sol_x)
+        # print('Optimal control inputs:', sol_u)
 
-        print('Optimal state trajectory:', sol_x)
-        print('Optimal control inputs:', sol_u)
-
+        return sol_u[0]
 
     # loss: l
     def l(self, z):
@@ -94,10 +115,9 @@ class MPC:
         u = z[self.N*self.nx:].reshape(self.N-1, self.nu)
         # return quadratic cost
         return jnp.sum(self.Q * x**2) + jnp.sum(self.R * (u**2))
-    
 
     # equality constraints: h
-    def h(self, z, x0=jnp.zeros(3)):
+    def h(self, z, x0):
         x = z[:self.N*self.nx].reshape(self.N, self.nx)
         u = z[self.N*self.nx:].reshape(self.N-1, self.nu)
 
@@ -127,17 +147,38 @@ class MPC:
 
         # stacking of equality constraints
         return jnp.hstack(cl) 
-
     
 if __name__ == "__main__":
+    
     f = dynamics.get("L_SIMO_RD3")
-    mpc = MPC(
-        N=3, nx=3, nu=1, ny=3, f=f
-    )
-    x0 = jnp.zeros(mpc.nx) # parameter for initial condition
-    x1 = jnp.ones(mpc.nx)
-    mpc(x0)
-    mpc(x1)
+    N = 10
+    nx = 3
+    nu = 1
+    mpc = MPC(N=N, nx=nx, nu=nu, ny=3, f=f)
+
+    # x0 = jnp.zeros(nx) # parameter for initial condition
+    # x1 = jnp.ones(nx)
+    # mpc(x0)
+    # mpc(x1)
+
+    # eval_data = 3.0 * np.random.randn(nx) generated the below
+    eval_data = jnp.array([1.59609801, 1.51405802, 4.63639117])
+
+    Q = 10.0                # state loss
+    R = 0.0001              # action loss
+    u_N, x_N = [], []
+    x = eval_data
+    loss = 0.0
+    for _ in range(10):
+        u = mpc(x)
+        x_kp1 = f(x, u)
+        loss += (R * jnp.sum(u**2) + Q * jnp.sum(x_kp1**2)) / N
+        x = x_kp1
+        u_N.append(u); x_N.append(x)
+    
+    u_N, x_N = jnp.stack(u_N), jnp.stack(x_N)
+    (R * jnp.sum(u_N**2) + Q * jnp.sum(x_N**2)) / N
+    print(f'loss: {loss}')
 
 
     print('fin')
